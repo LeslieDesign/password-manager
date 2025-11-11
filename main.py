2527464a8eac7b8706794e8a9aed32b9a00178e6
Leslie Middleton <lesliemddltn@gmail.com>
2025-11-11 15:22:39 -0500

    Update project with latest changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import string
 import secrets
 import pyperclip
+import json
 FONT_NAME = "Arial"
 FONT_SIZE = 9
 
@@ -11,29 +12,61 @@ def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(characters) for _ in range(length))
     pyperclip.copy(password)
-    pwd.delete(0,END)
-    pwd.insert(END, password)
+    pwd_entry.delete(0,END)
+    pwd_entry.insert(END, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_data():
-    url = website.get()
-    if not url:
-        messagebox.showerror("Validation Error", "Please enter a valid URL.")
+    website = website_entry.get()
     username = email.get()
-    if not username:
-        messagebox.showerror("Validation Error", "Please enter an email or username.")
-    password = pwd.get()
-    if not password:
-        messagebox.showerror("Validation Error", "Please enter a password.")
+    password = pwd_entry.get()
+    new_data = {
+        website: {
+            "email" : username,
+            "password" : password
+        }
+    }
+
+    if not password or not website:
+        messagebox.showerror("Validation Error", "Please make sure no fields are empty.")
     else:
-        is_ok = messagebox.askokcancel(title=url, message=f"Username = {username}\nPassword = {password}\n\nIs it ok to save?")
-        if is_ok:
-            with open("data_file.txt", "a") as data:
-                data.write(f"{url} | {username} | {password}\n")
-            website.delete(0, END)
-            pwd.delete(0, END)
-            website.focus()
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            pwd_entry.delete(0, END)
+            website_entry.focus()
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            username = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Username: {username}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,40 +82,44 @@ canvas.create_image(125,125, image=logo_img)
 website_lbl = Label(text="Website:", font=(FONT_NAME, FONT_SIZE))
 website_lbl.grid(column=0, row=1, sticky="e")
 
-website = Entry(width=43)
-website.focus()
-website.grid(column=1, row=1, columnspan=2, sticky="w")
+website_entry = Entry(width=23)
+website_entry.focus()
+website_entry.grid(column=1, row=1, sticky="ew", padx=5)
+
+search_btn = Button(text="Search", background="azure1", width=15,
+                activebackground="CadetBlue3",
+                activeforeground="white", command=find_password)
+search_btn.grid(column=2, row=1, sticky="e")
 
 #Row 2:
 email_lbl = Label(text="Email/Username:", font=(FONT_NAME, FONT_SIZE))
-email_lbl.grid(column=0, row=2, sticky="e")
+email_lbl.grid(column=0, row=2, sticky="w")
 
-email = Entry(width=43)
+email = Entry(width=30)
 email.insert(0, "yourname@gmail.com")
-email.grid(column=1, row=2, columnspan=2, sticky="w")
+email.grid(column=1, row=2, columnspan=2, sticky="ew", padx=5)
 
 #Row 3:
 pwd_lbl = Label(text="Password:", font=(FONT_NAME, FONT_SIZE))
 pwd_lbl.grid(column=0, row=3, sticky="e")
 
-pwd = Entry(width=23)
-pwd.grid(column=1, row=3, sticky="w")
+pwd_entry = Entry(width=23)
+pwd_entry.grid(column=1, row=3, sticky="ew", padx=5)
 
-generate_btn = Button(text="Generate Password", background="azure1",
+generate_btn = Button(text="Generate Password", background="azure1", width=15,
                 activebackground="CadetBlue3",
                 activeforeground="white", command=generate_password)
-generate_btn.grid(column=1, row=3, sticky="e")
+generate_btn.grid(column=2, row=3, sticky="w")
 
 #Row 4:
-add_btn = Button(text="Add to Manager", width=36, background="azure2",
+add_btn = Button(text="Add to Manager", width=36, background="black", fg="white",
                 activebackground="CadetBlue3",
                 activeforeground="white", command=save_data)
-add_btn.grid(column=1, row=4, columnspan=2, sticky="w")
+add_btn.grid(column=1, row=4, columnspan=3, sticky="ew", pady=10)
 
 
 canvas.grid(column=1, row=0)
 for i in range(4):
-    window.grid_rowconfigure(i, pad=3)
-
+    window.grid_rowconfigure(i, pad=4)
 
 window.mainloop()
